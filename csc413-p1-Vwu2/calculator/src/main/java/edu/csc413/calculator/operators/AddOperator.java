ac9dae4010a42d6eef9a1822aba32fb25a85ced1
Vincent Wu <vincentswu2@gmail.com>
2019-02-16 15:36:48 -0800

    Adding AddOperator class

---
+++ csc413-p1-Vwu2/calculator/src/main/java/edu/csc413/calculator/operators/AddOperator.java
@@ -0,0 +1,15 @@
+package java.edu.csc413.calculator.operators;
+
+public class AddOperator extends Operator {
+
+    public int priority(){
+        return 2;
+    }
+
+    public Operand execute(Operand first, Operand second){
+        int temp;
+        temp = first.value + second.value;
+        Operand result = new Operand(temp);
+        return result;
+    }
+}
